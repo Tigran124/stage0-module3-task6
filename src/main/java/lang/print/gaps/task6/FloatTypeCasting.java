09435c108c7906555fc8f6d3a6c67a85d1069bf2
Tigran124 <tigran.h.988@gmail.com>
2023-12-17 18:19:15 +0400

    changes committed

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        if (numberToBeRounded % (int)numberToBeRounded > 0.4){
+            System.out.println((int) numberToBeRounded + 1);
+        }else {
+            System.out.println((int) numberToBeRounded);
+        }
     }
 }
